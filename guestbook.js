11d061d69ea140d86dadf2d7d727c0e414fd0576
vbuendia <vicente.buendia@icloud.com>
2022-04-14 18:59:05 +0000

    apiUrl

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://f177ed41.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
